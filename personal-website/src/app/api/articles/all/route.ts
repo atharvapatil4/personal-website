020358b30e281ba0e4b8362fd0ef2a636cd0321a
Atharva Patil <atharva@Atharvas-MacBook-Pro-14.local>
2024-01-27 00:19:14 -0800

    final

--- personal-website/src/app/api/articles/all/route.ts
+++ personal-website/src/app/api/articles/all/route.ts
@@ -1,5 +1,6 @@
 // app/api/articles/GET.ts
 import fileList from "../../../../../fileList.json"; // Adjust the path as needed
+import matter from "gray-matter";
 
 export async function GET() {
   return new Response(JSON.stringify(fileList), {
--- personal-website/src/app/blog/page.tsx
+++ personal-website/src/app/blog/page.tsx
@@ -2,15 +2,11 @@
 "use client";
 import { useEffect, useState } from "react";
 import Link from "next/link";
-import Head from "next/head";
-
-interface BlogPost {
-  slug: string;
-  createdAt: string; // Adjust as per the structure of your JSON file
-}
+import { BlogPostSchema } from "../components/post";
+import PostPreview from "../components/post";
 
 export default function Blog() {
-  const [posts, setPosts] = useState<BlogPost[]>([]);
+  const [posts, setPosts] = useState<BlogPostSchema[]>([]);
 
   useEffect(() => {
     fetch("/api/articles/all")
@@ -19,28 +15,23 @@ export default function Blog() {
       .catch((error) => console.error("Fetching error:", error));
   }, []);
 
+  const postPreviews = posts
+    .slice() // Create a shallow copy to avoid mutating the original array
+    .sort((a, b) => {
+      // Parse the dates and compare them
+      const dateA = new Date(a.frontMatter.date).getTime();
+      const dateB = new Date(b.frontMatter.date).getTime();
+      return dateB - dateA; // Sort in descending order (most recent first)
+    })
+    .map((post) => <PostPreview key={post.slug} {...post} />);
+
   return (
-    <main className="flex min-h-screen flex-col items-center p-20 bg-gradient-radial">
-      <h1 className="font-semibold text-5xl">Blog</h1>
-      <ul className="w-full">
-        {posts.map((post) => (
-          <li
-            key={post.slug}
-            className="grid grid-cols-2 gap-0 p-0 place-items-center "
-          >
-            <Link
-              href={`/blog/${post.slug}`}
-              className="text-blue-500 hover:underline truncate"
-            >
-              {post.slug}
-            </Link>
-            <span className="text-right">
-              {new Date(post.createdAt).toDateString()}
-            </span>
-          </li>
-        ))}
-      </ul>
-      <h1 className="font-semibold">
+    <main className="min-h-screen p-28">
+      <h1 className="font-semibold text-center text-5xl italic py-6">Blog</h1>
+      <div className="grid grid-cols-1 gap-4 mx-auto max-w-2xl px-6">
+        {postPreviews}
+      </div>
+      <h1 className="font-semibold text-center py-6">
         {" "}
         <Link href="/" className=" text-blue-500 hover:underline italic">
           Back to Home
