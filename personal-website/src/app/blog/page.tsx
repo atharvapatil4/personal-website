3e8bd50be9475e74a6b4330369963c5a05c83e19
Atharva Patil <atharvapatil@berkeley.edu>
2025-03-01 23:34:21 -0500

    feat: cursor update

--- personal-website/src/app/blog/page.tsx
+++ personal-website/src/app/blog/page.tsx
@@ -1,46 +1,55 @@
-// app/blog/page.tsx
-"use client";
-import { useEffect, useState } from "react";
+import fs from "fs";
+import path from "path";
 import Link from "next/link";
-import { BlogPostSchema } from "../components/post";
 import PostPreview from "../components/post";
-import { PageWrapper } from "../pageWrapper";
+import { BlogPostSchema } from "../components/post";
+import matter from "gray-matter";
 
-export default function Blog() {
-  const [posts, setPosts] = useState<BlogPostSchema[]>([]);
+function formatDate(dateString: string) {
+  const date = new Date(dateString);
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const year = date.getFullYear();
 
-  useEffect(() => {
-    fetch("/api/articles/all")
-      .then((response) => response.json())
-      .then((data) => setPosts(data))
-      .catch((error) => console.error("Fetching error:", error));
-  }, []);
+  return `${month}-${day}-${year}`;
+}
 
-  const postPreviews = posts
-    .slice() // Create a shallow copy to avoid mutating the original array
-    .sort((a, b) => {
-      // Parse the dates and compare them
-      const dateA = new Date(a.frontMatter.date).getTime();
-      const dateB = new Date(b.frontMatter.date).getTime();
-      return dateB - dateA; // Sort in descending order (most recent first)
-    })
-    .map((post) => <PostPreview key={post.slug} {...post} />);
+export default async function Blog() {
+  const postsDirectory = path.join(process.cwd(), "public", "articles");
+  const filenames = fs.readdirSync(postsDirectory);
+
+  const posts: BlogPostSchema[] = filenames.map((filename) => {
+    const filePath = path.join(postsDirectory, filename);
+    const fileContents = fs.readFileSync(filePath, "utf8");
+    const matterResult = matter(fileContents);
+
+    return {
+      slug: filename.replace(/\.md$/, ""),
+      createdAt: formatDate(fs.statSync(filePath).birthtime.toString()),
+      frontMatter: matterResult.data,
+    };
+  });
+
+  // Sort posts by date (most recent first)
+  const sortedPosts = posts.sort((a, b) => {
+    const dateA = new Date(a.frontMatter.date).getTime();
+    const dateB = new Date(b.frontMatter.date).getTime();
+    return dateB - dateA;
+  });
 
   return (
     <main className="min-h-screen py-28 px-2">
       <h1 className="font-semibold text-center text-5xl italic py-6">Blog</h1>
 
       <div className="grid grid-cols-1 gap-6 mx-auto max-w-2xl px-2">
-        {/* Map black border line over each post */}
-        {postPreviews.map((postPreview) => (
-          <div key={postPreview.key} className="border-l-4 border-black pl-3">
-            {postPreview}
+        {sortedPosts.map((post) => (
+          <div key={post.slug} className="border-l-4 border-black pl-3">
+            <PostPreview {...post} />
           </div>
         ))}
       </div>
       <h1 className="font-semibold text-center py-8">
-        {" "}
-        <Link href="/" className=" text-blue-500 hover:underline italic">
+        <Link href="/" className="text-blue-500 hover:underline italic">
           Back to Home
         </Link>
       </h1>
